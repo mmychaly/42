7d45533c8d0b7b00b522fee78968eaf6a877070c
Mickael Mychalyszyn <mmychaly@f2r5s19.clusters.42paris.fr>
2025-09-27 18:22:06 +0200

    last add

--- projects/inception/srcs/requirements/mariadb/conf/create_db.sh
+++ projects/inception/srcs/requirements/mariadb/conf/create_db.sh
@@ -2,9 +2,7 @@
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
 
-        chown -R mysql:mysql /var/lib/mysql
-
-        mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
+        mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
 fi
 
@@ -18,11 +16,12 @@ DROP DATABASE test;
 DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';
-CREATE DATABASE ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE ${DB_NAME};
 CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_PASS}';
-GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
         /usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql
         rm -f /tmp/create_db.sql
 fi
+exec /usr/bin/mysqld --user=mysql --console
