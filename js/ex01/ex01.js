375f1ec1835748381595bfdf5793d7e09a0f6b02
mmychaly <mmychalyszyn@gmail.com>
2026-03-31 04:18:31 +0200

    and lasr change

--- js/ex01/ex01.js
+++ js/ex01/ex01.js
@@ -1,1 +1,6 @@
-console.log("Hello World!\nDisplay from file.js")
+"use strict";
+console.log("Hello World!\nDisplay from file.js");
+console.log(obj);
+
+document.getElementById("test_1").textContent = "Hello Word";
+document.getElementById("test_2").textContent = "Display from file.js"
