746801922b9fae1f9a03a47f8a96e75e846402d0
mmychaly <mmychalyszyn@gmail.com>
2026-03-31 06:16:31 +0200

    last in ex03

--- js/ex03/ex03.js
+++ js/ex03/ex03.js
@@ -11,7 +11,31 @@ console.log(num, str, char, double, obj);
 let docObj1 = document.getElementById("test1");
 let docObj2 = document.getElementById("test2");
 let docObj3 = document.getElementById("test3");
+let docObj4 = document.getElementById("test4");
+let docObj5 = document.getElementById("test5");
+let docObj6 = document.getElementById("test6");
 
-docObj1.textContent = obj.key;
+docObj1.textContent = obj;
 docObj2.textContent = obj.num;
 docObj3.textContent = str;
+
+str = "New_string";
+docObj4.textContent = str;
+
+const constStr = "Hello";
+docObj5.textContent = constStr;
+
+// constStr = "NewConstStr"; //invalid assignment to const 'constStr'
+// docObj6.textContent = constStr;
+// console.log(constStr);
+
+var varObj = obj;
+docObj6.textContent = varObj.key;
+
+let und;
+console.log(und);//undefined
+
+str = `Combinaison de strings , ${constStr}`;
+console.log(str);
+console.log("objkey".length);
+console.log("objkey".toUpperCase());
