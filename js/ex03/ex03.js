bf0037f8a3060270be49242e5b3171d3b06c785f
mmychaly <mmychalyszyn@gmail.com>
2026-04-06 01:43:24 +0200

    refactoring

--- js/ex03/ex03.js
+++ js/ex03/ex03.js
@@ -1,41 +1,48 @@
 "use strict"
-
+//Initialisation de variables et const avec differents types
 let num = 5;
 let str = "Salut";
-let char = 'c';
 let double = 0.5;
-let obj = { key: "stringObj", num: 5};
+let bool = true;
+let und;
+let obj = { name: "mika", 
+			str: "Hello Word!",
+			num: 34,
+			helloWord() {
+				console.log("Hello Word!");
+			}//Pas de parenthèses  en derniere element
+		};
 
-console.log(num, str, char, double, obj);
+const constStr = "Mika";
 
+var varObj = obj;
+
+console.log("Let: ", und, num, str, bool, double, obj);
+console.log("Const: ", constStr);
+console.log("Var: ", varObj.name, varObj.str);
+
+
+//Display on the page  + work with string methods
 let docObj1 = document.getElementById("test1");
 let docObj2 = document.getElementById("test2");
 let docObj3 = document.getElementById("test3");
 let docObj4 = document.getElementById("test4");
 let docObj5 = document.getElementById("test5");
 let docObj6 = document.getElementById("test6");
+let docObj7 = document.getElementById("test7");
+let docObj8 = document.getElementById("test8");
+let docObj9 = document.getElementById("test9");
 
-docObj1.textContent = obj;
+obj.helloWord();
+docObj1.textContent = obj.name;
 docObj2.textContent = obj.num;
-docObj3.textContent = str;
-
-str = "New_string";
-docObj4.textContent = str;
-
-const constStr = "Hello";
-docObj5.textContent = constStr;
-
-// constStr = "NewConstStr"; //invalid assignment to const 'constStr'
-// docObj6.textContent = constStr;
-// console.log(constStr);
-
-var varObj = obj;
-docObj6.textContent = varObj.key;
-
-let und;
-console.log(und);//undefined
+docObj3.textContent = obj.str;
 
 str = `Combinaison de strings , ${constStr}`;
 console.log(str);
-console.log("objkey".length);
-console.log("objkey".toUpperCase());
+console.log(str.length);
+console.log(obj.name.length);
+console.log(obj.name.toUpperCase());
+console.log(obj.helloWord());
+
+
