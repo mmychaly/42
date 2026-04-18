41da8eed1491c0ccb9b522e47230004fc00cff88
mmychaly <mmychalyszyn@gmail.com>
2026-04-18 02:31:20 +0200

    Test alert,prompt,confirm

--- js/ex05/ex05.js
+++ js/ex05/ex05.js
@@ -1,25 +1,22 @@
 "use strict"
 
 
-let array = {
-	user1: { name: Mika, age: 34},
-	user2: { name: Alizée, age: 32},
-	};
+let docObject = document.getElementById("test2");
+let startBtn = document.getElementById("startBtn")
 
-//Display on the page  
-let docObj1 = document.getElementById("test1");
-let docObj2 = document.getElementById("test2");
-let docObj3 = document.getElementById("test3");
-let docObj4 = document.getElementById("test4");
-let docObj5 = document.getElementById("test5");
-let docObj6 = document.getElementById("test6");
-let docObj7 = document.getElementById("test7");
-let docObj8 = document.getElementById("test8");
-let docObj9 = document.getElementById("test9");
+startBtn.addEventListener("click", () => {
+	let message = alert("Merci d'accepter!");
+	let isBoolen = confirm("Do you agree transmit information?");
+	if (isBoolen)
+	{
+		let age = prompt("How old are you?");
+		if (age != null && age != "")
+			docObject.textContent = "You answered: " + age;
+	}
+	else
+		docObject.textContent= "You didn't respond";
+});
 
-docObj1.textContent = array;
-docObj2.textContent = array[0];
-docObj3.textContent = array[1];
-docObj4.textContent = array[2];
+
 
 
