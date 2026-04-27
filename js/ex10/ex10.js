6f9a11255bbf7260422c97cba331cbe449cc943e
mmychaly <mmychalyszyn@gmail.com>
2026-04-27 04:29:13 +0200

    Ex10 destructuring in function. array + object

--- js/ex10/ex10.js
+++ js/ex10/ex10.js
@@ -26,10 +26,10 @@ objTest3.textContent = city;
 
 let data = ["Hksdi", "Odskd", "ipIOppo"];
 
-let [fisrt, seconde, third] = data;
+let [fisrt, second, third] = data;
 
 objTest4.textContent = fisrt;
-objTest5.textContent = seconde;
+objTest5.textContent = second;
 objTest6.textContent = third;
 
 let userUp = {
@@ -51,29 +51,43 @@ let [newFirst, ...array]= data;
 console.log(`in newFirst: ${newFirst}, in array: ${array}`);
 
 
+let dataUp = [
+	{user: "Mika", age: 34},
+	{user: "Alizée", age: 32}
+];
+
+let [{user: user1, age: ageUser1}, {user: user2, age: ageUser2}] = dataUp;
+
+console.log(`DataUp. First user: ${user1}, ${ageUser1}. Second user: ${user2}, ${ageUser2}`);
+
+
 
 
 
 
 console.log("\nUse destructuring in function");
 console.log("\nDisplay value of array from arguments of fuction");
-function getData([firstArg, secondeArg, thirdArg] = [0,0,0])
-{
-	console.log(`Fisrt value: ${firstArg}, seconde value: ${secondeArg}, third value: ${thirdArg}`);
-}
-getData(data);
-getData();
 
+function getData([firstArg, secondArg, thirdArg] = [0,0,0])
+{
+	console.log(`Function getData.Fisrt value: ${firstArg}, second value: ${secondArg}, third value: ${thirdArg}`);
+}
+console.log("Function with args");
+getData(data);
+console.log("Function without args");
+getData();
 
 
 
 console.log("\nDisplay value of object from arguments of fuction");
 function simpleUser({name: nameUser, age: ageUser, city: cityUser} = {})
 {
-	console.log(`FUnction simpleUser : Name: ${nameUser}, age: ${ageUser}, city: ${cityUser}`);
+	console.log(`Function simpleUser : Name: ${nameUser}, age: ${ageUser}, city: ${cityUser}`);
 }
 
+console.log("Function with args");
 simpleUser(user);
+console.log("Function without args");
 simpleUser();
 
 
@@ -81,5 +95,17 @@ function hardUser({name = "Anonymous", age = 99, city = "Unknown", other: {adres
 {
 	console.log(`Function hardUser: Name: ${name}, age: ${age}, city: ${city}, other: ${adress}`);
 }
+console.log("\nFunction with args");
 hardUser(userUp);
+console.log("Function without args");
 hardUser();
+
+console.log("\nDisplay value of array with objects from arguments in fuction");
+function displayDataUp([{user: firstUser = "Anonimus", age: firstUserAge = "Unknowe"} = {}, {user: secondUser = "Anonimus", age: secondUserAge = "Unknowe"} = {}] = [])
+{
+	console.log(`Display from displayDataUp.\nFirst user: ${firstUser}, age ${firstUserAge}.\nSecond user: ${secondUser}, age ${secondUserAge}`)
+}
+console.log("Function with args");
+displayDataUp(dataUp);
+console.log("Function without args");
+displayDataUp();
