9eb238d8ed233a4b09f6c2f8d0d40172b51069f5
mmychaly <mmychalyszyn@gmail.com>
2026-04-29 02:22:40 +0200

    Ex12 : v.1 func nativ call, apply, bind

--- js/ex12/ex12.js
+++ js/ex12/ex12.js
@@ -31,5 +31,48 @@ counter.getValue();
 
 testObj.textContent = `Value in func propertie: ${counter.value}`;
 
+//Nativ properies and methodes
+objTest2.textContent = `Propertie .name: ${counter.name}`;
+objTest3.textContent = `Propertie .length: ${counter.length}`;
 
 
+const user_1 = {
+	name: "Mika",
+	age: 34
+};
+
+const user_2 = {
+	name: "Alizée",
+	age: 32
+};
+
+function displayUser(msg)
+{
+	return `${msg}: ${this.name} ${this.age}`;
+}
+
+//call
+
+objTest4.textContent = displayUser.call(user_1, "First user");
+objTest5.textContent = displayUser.call(user_2, "Second user");
+
+//apply
+
+let args = ["Msg from array. ", "User"];
+function displayUserArray(msg)
+{
+	return `${msg}: ${this.name} ${this.age}`;
+}
+objTest6.textContent = displayUserArray.apply(user_1, [args]);
+objTest7.textContent = displayUserArray.apply(user_2, ["Other msg from array. User"]);
+
+//bind
+
+let func_1 = displayUser.bind(user_1, "Launch func_1 with bind. User");
+let func_2 = displayUser.bind(user_2, "Launch func_2 with bind. User");
+
+console.log("\nLaunch func with bind");
+console.log(func_1());
+console.log(func_2());
+console.log(func_1());
+console.log(func_2());
