9b396b00d04b1fb4abe75db5f18bab0516e90658
mmychaly <mmychalyszyn@gmail.com>
2026-05-01 04:14:14 +0200

    Test setTimeout/setInterval

--- js/ex13/ex13.js
+++ js/ex13/ex13.js
@@ -8,5 +8,21 @@ let printMsg = new Function("msg", "return msg;");
 testObj.textContent = printMsg('Hello');
 
 
+let printMsgCons = new Function("msg", "console.log(msg);");
 
 
+setTimeout(printMsgCons.bind(null, 'Salut'), 1500);
+console.log("Use setTimeout with bind");
+
+let idSt = setTimeout(printMsgCons.bind(null, 'He'), 5000);
+console.log("Use clearTimeout with bind");
+setTimeout(() => clearTimeout(idSt), 1000);
+
+setTimeout(console.log.bind(null, "Use setInterval with bind"), 1500);
+
+let counter = 0;
+let idI = setInterval(() => {
+							counter++;
+							console.log(`Holla ${counter}`);
+ }, 2000);
+setTimeout(() => clearInterval(idI), 10000);
