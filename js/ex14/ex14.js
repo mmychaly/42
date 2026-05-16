f06588c99fc07faef23502472b482d283ff3e059
mmychaly <mmychalyszyn@gmail.com>
2026-05-16 04:09:22 +0200

    last add of promise, async + await

--- js/ex14/ex14.js
+++ js/ex14/ex14.js
@@ -1,42 +1,40 @@
 "use strict";
 
-
-let prom = new Promise((resolve, reject) => resolve("Launch callback in microtask"));
-
 console.log("Test promise.Befor");
+let prom = new Promise((resolve, reject) => resolve("Launch callback from promis in microtask"));
 prom.then((result) => console.log(result));
 console.log("Test promise.After");
-console.log("Console.log prom ", prom);
-let newProm = prom.then(result => result);
-console.log("Console.log newProm ", newProm);
 
-console.log("\nFast promise syntax");
+let newProm = prom.then(result => `Use first promise for create seconde, ${result}`); //Return new promise
+newProm.then(r => console.log("Launch new promise: " + r));
 
-Promise.resolve("My message from promise").then(r => console.log(r));
+setTimeout(() => {
+	Promise.resolve("\nLunch with, fast promise syntax").then(r => console.log(r));//Promise.resolve() == status fulfiled
 
-let errProm = new Promise((resolve, reject) => reject("We have a problem!"));
+	Promise.resolve("Test reject promise").then(r => console.log(r));
+	let errProm = new Promise((resolve, reject) => reject("We have a problem!"));//Status reject
 
-errProm.then(console.log("errProm in then"))
-		.catch(p => console.log("errProm in catch " + p));
+	errProm.then(r =>console.log("Error in then" + r))
+			.catch(p => console.log("Error in catch " + p));//We execute console.log in catch
+},2000);
 
-
-
+setTimeout(() => {
 function promFunc ()
 {
-	setTimeout(() => {
-		let promisNew = new Promise((resolve, reject) => resolve("Launch promis in promFunc"));
-		promisNew.then(result => console.log(result));
-	}, 3000);
+	console.log("Launch promFunc in await")
 	return new Promise((resolve, rejcet) => {
-		setTimeout(() => resolve("Salut from promFunc"), 2000);
+		setTimeout(() => resolve("Salut it's promis from promFunc"), 2000);
 	});
 }
 
 async function asyncFunc() {
-	console.log("in asyncFunc befor await");
+	console.log("\nTest async function.");
+	console.log("In async befor await");
 	let promAsync = await promFunc();
-	console.log("in asyncFunc ", promAsync);
-	console.log("in asyncFunc after await");
+	console.log("In async function ", promAsync);
+	console.log("In async function after await");
 }
 
 asyncFunc();
+
+ }, 4000);
