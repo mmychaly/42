3cb89bbfac011b3dcf480ed1b897f04cf23a00eb
mmychaly <mmychalyszyn@gmail.com>
2026-05-16 02:50:30 +0200

    async + await

--- js/ex14/ex14.js
+++ js/ex14/ex14.js
@@ -1,5 +1,6 @@
 "use strict";
 
+
 let prom = new Promise((resolve, reject) => resolve("Launch callback in microtask"));
 
 console.log("Test promise.Befor");
@@ -18,3 +19,24 @@ let errProm = new Promise((resolve, reject) => reject("We have a problem!"));
 errProm.then(console.log("errProm in then"))
 		.catch(p => console.log("errProm in catch " + p));
 
+
+
+function promFunc ()
+{
+	setTimeout(() => {
+		let promisNew = new Promise((resolve, reject) => resolve("Launch promis in promFunc"));
+		promisNew.then(result => console.log(result));
+	}, 3000);
+	return new Promise((resolve, rejcet) => {
+		setTimeout(() => resolve("Salut from promFunc"), 2000);
+	});
+}
+
+async function asyncFunc() {
+	console.log("in asyncFunc befor await");
+	let promAsync = await promFunc();
+	console.log("in asyncFunc ", promAsync);
+	console.log("in asyncFunc after await");
+}
+
+asyncFunc();
