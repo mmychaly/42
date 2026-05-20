add09385461af29db5c6cf67d3499560f1ce8b7a
mmychaly <mmychalyszyn@gmail.com>
2026-05-20 22:37:40 +0200

    Class

---
+++ js/ex18/ex18.js
@@ -0,0 +1,54 @@
+"use strict";
+
+
+const testObj = document.getElementById("test1");
+const testObj2 = document.getElementById("test2");
+
+class User{
+
+	rules = false; //Simple property
+	#privateRule = false;
+
+	constructor(name, age, email)
+	{
+		this.name = name;
+		this.age = age;
+		this.email = email;
+	}
+
+	sayHi() {console.log(`Hello, my name is ${this.name}`)};//Method of class in User.prototype
+	showUser() { return `User: ${this.name}, ${this.age}, ${this.email}`};
+
+	#showPrivateRule() {return this.#privateRule};
+	#setPrivateRule(value) {
+		if (value == true || value == false)
+			this.#privateRule = value;
+	};
+
+	usePrivateMethod() {console.log(this.#showPrivateRule())};
+	setPrivateMethod(value) {this.#setPrivateRule(value)};
+	
+	get userRules() {console.log(`User ${this.name} has rules: ${this.rules}`)}; //Getter for display value
+	set userRules(value) {
+		if (value == true)
+			this.rules = value;
+	} ;//Setter for set a value
+}
+
+let newUser = new User("Mika", 34, "mmychaly@dfdfdf.com");
+let user2 = new User("Alizée", 32, "a_alizée@ffsfsfs.com");
+
+newUser.sayHi();
+//newUser.showRules();
+newUser.userRules; //Launch getter. We don't make value
+newUser.userRules = true; // For launch setter ae need add a value
+newUser.userRules;
+
+testObj.textContent = newUser.showUser();
+testObj2.textContent = user2.showUser();
+
+console.log("Display simple property ", newUser.rules); //We can display property simple == true
+console.log("Try acces for user privateRule: ", newUser.privateRule);
+newUser.usePrivateMethod();
+newUser.setPrivateMethod(true);
+newUser.usePrivateMethod();
