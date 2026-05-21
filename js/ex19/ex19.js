27c74aac68b72921c9c93bb87f51002129ce1630
mmychaly <mmychalyszyn@gmail.com>
2026-05-21 21:38:38 +0200

    Class.Mixins

---
+++ js/ex19/ex19.js
@@ -0,0 +1,66 @@
+"use strict";
+
+class AnimalOwner{
+	constructor(name)
+	{
+		this.name = name;
+	}
+
+	showAnimalOwner() {console.log(`Owner of animal is ${this.name}`)};
+}
+
+let animalAction = {
+	goEat() {console.log(`${this.petName} eat!`)},
+	goWalk() {console.log(`${this.petName} walk!`)}
+}
+
+
+
+class Cat extends AnimalOwner{
+	constructor(name, petName, color){
+		super(name);
+		this.petName = petName;
+		this.color = color;
+	}
+
+	showCat() {console.log(`Cat name is ${this.petName}, his color is: ${this.color}`)};
+}
+
+class Dog extends AnimalOwner{
+	constructor(name, petName, color){
+		super(name);
+		this.petName = petName;
+		this.color = color;
+	}
+
+	showDog() {console.log(`Dog name is ${this.petName}, his color is: ${this.color}`)};
+}
+
+Object.assign(Dog.prototype, animalAction);
+Object.assign(Cat.prototype, animalAction);
+
+let myDog = new Dog("Mika", "Bloody", "black");
+let newDog = new Dog("Alizée", "Pig", "white");
+
+let myCat = new Cat("Mika", "Lolo", "white");
+let newCat = new Cat("Alizée", "Damnf", "brown");
+
+myDog.showDog();
+myDog.showAnimalOwner();
+myDog.goEat();
+myDog.goWalk();
+
+newDog.showDog();
+newDog.showAnimalOwner();
+newDog.goEat();
+newDog.goWalk();
+
+myCat.showCat();
+myCat.showAnimalOwner();
+myCat.goEat();
+myCat.goWalk();
+
+newCat.showCat();
+newCat.showAnimalOwner();
+newCat.goEat();
+newCat.goWalk();
