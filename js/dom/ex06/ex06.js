f7ae334d57292ac6b398a7df58e08efce78ee4e7
mmychaly <mmychalyszyn@gmail.com>
2026-06-11 02:24:50 +0200

    DOM. Test scrollBy + scrollTo

--- js/dom/ex06/ex06.js
+++ js/dom/ex06/ex06.js
@@ -12,3 +12,13 @@ console.log("ScrollTop: ", document.documentElement.scrollTop);
 
 console.log("PageXOffset: ", window.pageXOffset);
 console.log("PageYOffset: ", window.pageYOffset);
+
+console.log("Use scrollBy and scrollTo");
+let scroll1 = setInterval(() => {
+	window.scrollBy(0, 100);
+	if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight)
+	{
+		setTimeout(() => window.scrollTo(0, 0));
+		clearInterval(scroll1);
+	}		
+}, 200);
