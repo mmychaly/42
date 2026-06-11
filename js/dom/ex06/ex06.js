a305ba8c20d0bc944488cbcae87d53d49ac06ef0
mmychaly <mmychalyszyn@gmail.com>
2026-06-11 02:12:50 +0200

    DOM metrique HTML

---
+++ js/dom/ex06/ex06.js
@@ -0,0 +1,14 @@
+"use strict";
+
+console.log('Use metrique for html page');
+console.log("ClientWidth: ", document.documentElement.clientWidth);
+console.log("ClientHeight: ", document.documentElement.clientHeight);
+
+console.log("InnerWidth: ", window.innerWidth);
+console.log("InnerHeight: ", window.innerHeight);
+
+console.log("ScrollHeight: ", document.documentElement.scrollHeight);
+console.log("ScrollTop: ", document.documentElement.scrollTop);
+
+console.log("PageXOffset: ", window.pageXOffset);
+console.log("PageYOffset: ", window.pageYOffset);
