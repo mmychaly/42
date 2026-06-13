467d0de95449d55ad0b1532442cbeb84e33ba0c9
mmychaly <mmychalyszyn@gmail.com>
2026-06-13 02:03:55 +0200

    addEventListener with named function + onclick

--- js/dom/ex08/ex08.js
+++ js/dom/ex08/ex08.js
@@ -2,13 +2,30 @@
 
 const inputTest = document.querySelector("#test2");
 
-//console.log(inputTest.value);
+//Use addEventListener with function anonymous
 
 inputTest.addEventListener("input", (event) => {
-//	console.log(inputTest.value);
-	console.log(event);
+	console.log(inputTest.value);
 });
 
-const btn = document.querySelector("button");
+//Use addEventListener with named function
+const btn = document.querySelector("#btn1");
 
-btn.addEventListener("click", (event) => console.log(event));
+function diplayEvent(event)
+{
+	console.log("Type: ", event.type);
+	console.log("Target: ", event.target);
+	console.log("X: ", event.clientX, " | ", "Y: ", event.clientY);
+}
+
+btn.addEventListener("click", diplayEvent);
+
+
+//Use Property DOM onclick
+
+const btn3 = document.querySelector("#btn3");
+
+function sayHello(){
+	console.log("Hello from .js")
+}
+btn3.onclick = sayHello;
