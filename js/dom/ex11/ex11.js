376a59b8754792ad3aa85f581c2c58242b3a1083
mmychaly <mmychalyszyn@gmail.com>
2026-06-15 22:19:37 +0200

    mousedown mouseup click dbldclick mousemove

---
+++ js/dom/ex11/ex11.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const articleTest = document.querySelector("article");
+const test = document.querySelector("p");
+
+document.addEventListener("mousedown", (event) => 
+	{
+		console.log(`Event: ${event.type}, Target: ${event.target.tagName}`);
+});
+
+document.addEventListener("mouseup", (event) => 
+	{
+		console.log(`Event: ${event.type}, Target: ${event.target.tagName}`);
+});
+
+document.addEventListener("click", (event) => 
+	{
+		console.log(`Event: ${event.type}, Target: ${event.target.tagName}`);
+});
+
+document.addEventListener("dblclick", (event) => 
+	{
+		console.log(`Event: ${event.type}, Target: ${event.target.tagName}`);
+});
+
+document.addEventListener("mousemove", event =>
+	{
+		console.log(`Event: ${event.type}, Target: ${event.target.tagName}, X: ${event.clientX} Y: ${event.clientY}`);
+});
