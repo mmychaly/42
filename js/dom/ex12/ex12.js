c03cf7c3895f4e6605fd2dac163581228158d6f4
mmychaly <mmychalyszyn@gmail.com>
2026-06-17 19:40:33 +0200

    DOm form. forms/elements

--- js/dom/ex12/ex12.js
+++ js/dom/ex12/ex12.js
@@ -1,14 +1,18 @@
 "use strict";
 
 const form_1 = document.forms[0];
-const form_2 = document.forms.form_2;
+const form_2 = document.forms.secondForm;
 
 form_1.addEventListener("submit", event => {
+	event.preventDefault();
 	console.log("Form 1")
-	console.log(`Input text: ${form_1.elements.name} == ${form_1.elements.name.value}`);		console.logconsole.log(`Input checkbox: ${form_1.elements.input_2} == ${form_1.elements.input_2.checked}`);
+	console.log(`Input text: ${form_1.elements.name.name} == ${form_1.elements.name.value}`);		
+	console.log(`Input checkbox: ${form_1.elements.input_2.id} == ${form_1.elements.input_2.checked}`);
 });
 
 form_2.addEventListener("submit", event => {
+	event.preventDefault();
 	console.log("Form 2")
-	console.log(`Input text: ${form_1.elements.name} == ${form_1.elements.name.value}`);		console.logconsole.log(`Input checkbox: ${form_1.elements.input_2} == ${form_1.elements.input_2.checked}`);
+	console.log(`Input text: ${form_2.elements.email.name} == ${form_2.elements[0].value}`);		
+	console.log(`Input checkbox: ${form_2.elements.input_4.id} == ${form_2.elements["input_4"].checked}`);
 });
