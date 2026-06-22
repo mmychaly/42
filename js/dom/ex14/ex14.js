f66c8ce3a9da5f44fb25052afbe2479ae2a0a9ba
mmychaly <mmychalyszyn@gmail.com>
2026-06-22 02:00:13 +0200

    DOM form event input change

--- js/dom/ex14/ex14.js
+++ js/dom/ex14/ex14.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const myForm = document.querySelector("#userData");
+
+myForm.addEventListener("input", e => {
+	if (e.type === "input" && e.target.name === "name")
+	{
+		console.log("input event: ", e.type);
+		let data = new FormData(myForm);
+		console.log(data.get("name"));
+	}
+});
+
+myForm.addEventListener("change", e => {
+	if (e.type === "change" && e.target.name === "email")
+	{
+		console.log("change event");
+		let data = new FormData(myForm);
+		console.log(data.get("email"));
+	}
+});
