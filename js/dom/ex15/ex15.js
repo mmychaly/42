4fd759077a4fdbd57fa4bd51e78b5747948a22b2
mmychaly <mmychalyszyn@gmail.com>
2026-06-23 22:45:53 +0200

    add events load html

--- js/dom/ex15/ex15.js
+++ js/dom/ex15/ex15.js
@@ -1,2 +1,27 @@
 "use strict";
 
+const test = document.querySelector("#test3");
+
+test.textContent = "Hello";
+
+document.addEventListener("DOMContentLoaded", e => {
+	console.log("DOMContentLoaded event");
+	console.log(document.readyState);
+});
+
+window.addEventListener("load", e => {
+	console.log("Load event");
+	console.log(document.readyState);
+});
+
+document.addEventListener("readystatechange", e => {
+	console.log("readystatechange event");
+	console.log(document.readyState);
+});
+
+window.addEventListener("beforeunload", e => {
+	console.log("beforeunload event");
+	e.preventDefault();
+	e.returnValue = "";
+});
+
