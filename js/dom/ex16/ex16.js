3809266bd1b63ce6fdbc25338993fac6e020581d
mmychaly <mmychalyszyn@gmail.com>
2026-06-25 05:04:35 +0200

    DOM preloading img, new Image()

--- js/dom/ex16/ex16.js
+++ js/dom/ex16/ex16.js
@@ -1,2 +1,24 @@
 "use strict";
 
+const test = document.querySelector("#test3");
+const pImg = document.querySelector("#pImg");
+
+test.addEventListener("click", e => {
+	console.log("click déclenché !");
+	let imgTest = new Image();
+
+    imgTest.addEventListener("load", () => {
+        console.log("Image chargée");
+    });
+
+    imgTest.addEventListener("error", () => {
+        console.log("Image non trouvée");
+        console.log(imgTest.src); 
+    });
+	imgTest.src = "astonavt.jpg";
+	imgTest.width = 200;
+	imgTest.height = 200;
+	test.before(imgTest);
+});
+
+//	pImg.append(imgTest);
