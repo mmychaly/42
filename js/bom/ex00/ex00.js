4c2b72b82fa14422670d3f36d64c579bf0d6f2f6
mmychaly <mmychalyszyn@gmail.com>
2026-06-26 05:33:26 +0200

    BOM element de BOM

--- js/bom/ex00/ex00.js
+++ js/bom/ex00/ex00.js
@@ -1,7 +1,16 @@
 "use strict";
 
-window.addEventListener("resize", () => console.log(window.innerWidth, window.innerHeight));
-window.addEventListener("scroll", () => console.log(window.scrollY));
+window.addEventListener("resize", () => console.log("Sise of window: ", window.innerWidth, window.innerHeight));
+window.addEventListener("scroll", () => console.log("Scroll position: ", window.scrollY));
 
 window.addEventListener("online", () => console.log("Connected"));
 window.addEventListener("offline", () => console.log("Disconected"));
+
+console.log("location: ", location);
+console.log("navigation: ", navigator);
+console.log("history: ", history);
+console.log("screen: ", screen);
+console.log("document: ", document);
+console.log("localStorage: ", localStorage);
+console.log("sessionStorage: ", sessionStorage);
+
