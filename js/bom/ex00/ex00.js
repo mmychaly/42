3c5db23dc091dca3e1560aacc468db7599b4df7f
mmychaly <mmychalyszyn@gmail.com>
2026-06-26 05:18:40 +0200

    BOM events scroll online/offline

--- js/bom/ex00/ex00.js
+++ js/bom/ex00/ex00.js
@@ -1,3 +1,7 @@
 "use strict";
 
 window.addEventListener("resize", () => console.log(window.innerWidth, window.innerHeight));
+window.addEventListener("scroll", () => console.log(window.scrollY));
+
+window.addEventListener("online", () => console.log("Connected"));
+window.addEventListener("offline", () => console.log("Disconected"));
