db298ddf320e8662f5210445b11b45ba7a281db7
mmychaly <mmychalyszyn@gmail.com>
2026-06-30 06:18:44 +0200

    navigator userAgent language languages onLine geolocation cookieEnable

--- js/bom/ex04/ex04.js
+++ js/bom/ex04/ex04.js
@@ -1,2 +1,14 @@
 "use strict"
 
+console.log(navigator.userAgent);
+console.log(navigator.language);
+console.log(navigator.languages);
+console.log(navigator.onLine);
+
+navigator.geolocation.getCurrentPosition(position => {
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
+	console.log("accurance", position.coords.accuracy);
+});
+
+console.log(navigator.cookieEnabled);
