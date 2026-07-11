074114008e0d0e82d8592e210679e4180b3ad0d8
mmychaly <mmychalyszyn@gmail.com>
2026-07-11 05:43:13 +0200

    create first composent

--- js/react/react_composant/trainReactComposant/src/App.jsx
+++ js/react/react_composant/trainReactComposant/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-
+import SendHello from './SendHello'
 function App() {
-
+  return (<SendHello />)
 }
 
 export default App
---
+++ js/react/react_composant/trainReactComposant/src/SendHello.jsx
@@ -0,0 +1,5 @@
+function SendHello(){
+	return <p>Hello from my first composent</p>
+}
+
+export default SendHello;
