cd47ebcdf3d9266b49b8a535422469a52f34a928
mmychaly <mmychalyszyn@gmail.com>
2026-07-21 20:26:38 +0200

    use onChange and form

--- js/react/useEvent/src/InputComp.jsx
+++ js/react/useEvent/src/InputComp.jsx
@@ -5,18 +5,37 @@ function InputFunc({handleChange})
 	return <input onChange={handleChange} />
 }
 
+function FormFunc({handleChange})
+{
+	return (
+			<form onSubmit={handleChange}>
+				<input />
+				<button type="submit">Submit</button>
+			</form>
+	)
+}
+
 function InputComp()
 {
 	const [text, setText] = useState("Mika");
-	function handleChange (event)
+	function handleChangeInput (event)
 	{
+		console.log("handleChangeInput");
 		setText(event.target.value);
+	}
+
+	function handleChangeForm (event)
+	{
+		event.preventDefault();
+		console.log("handleChangeForm");
+		setText(event.target.elements[0].value);
 	}
 
 	return (
 		<>
 			<h1>Use event react</h1>
-			<InputFunc handleChange={handleChange}/>
+			<InputFunc handleChange={handleChangeInput}/>
+			<FormFunc handleChange={handleChangeForm}/>
 			<p>{text}</p>
 		</>
 	)
