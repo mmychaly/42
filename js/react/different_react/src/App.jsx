bb850236107c4803970ee06098d07c58775e06ce
mmychaly <mmychalyszyn@gmail.com>
2026-07-23 22:18:48 +0200

    rendering conditionnal

---
+++ js/react/different_react/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+
+function App() {
+  return (
+          <>
+              <h1 style={{textAlign:"center"}}>Conditional rendering</h1>
+          </>
+  );
+}
+
+export default App
