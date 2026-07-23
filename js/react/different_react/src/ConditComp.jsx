19cf9f90db1145c47cba662398f9537eded8724e
mmychaly <mmychalyszyn@gmail.com>
2026-07-23 23:19:55 +0200

    variable intemediare

--- js/react/different_react/src/ConditComp.jsx
+++ js/react/different_react/src/ConditComp.jsx
@@ -8,15 +8,33 @@ function ConditComp()
 	{
 		setNumber(number + 1);
 	}
+	let message ;
+	if(number === 10)
+	{
+		message = <>
+			<button onClick={handlerNumber}>+1</button>
+			<p>Number equal ten!</p>
+		</>;
+	}
 
-	return (
-		<>
+	else if (number === 15)
+	{
+		message = <>
+			<button onClick={handlerNumber}>+1</button>
+			<p>Number equal Fifteen!</p>
+		</>;
+	}
+	else
+	{
+		message = <>
 			<button onClick={handlerNumber}>+1</button>
 			<p>{number} est {number % 2 === 0 ? "pair": "impair"}</p>
 			<p>{number % 2 === 0 && "It's really pair!"}</p>
 		</>
+	}
 
-	);
+
+	return ( <>{message}</>);
 }
 
 export default ConditComp;
