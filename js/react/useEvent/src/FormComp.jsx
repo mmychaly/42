fd99b3ef70a00c30e0adbc245cb2aa385df41722
mmychaly <mmychalyszyn@gmail.com>
2026-07-23 02:00:03 +0200

    add other form whise array in state

--- js/react/useEvent/src/FormComp.jsx
+++ js/react/useEvent/src/FormComp.jsx
@@ -1,40 +1,52 @@
 import {useState} from 'react';
 
-function FormFunc({handlerInputChange, handlerSubmit})
-{
-	return (
-		<>
-			<form onSubmit={handlerSubmit}>
-				<input onChange={handlerInputChange}/>
-				<button type="submit">Submit</button>
-			</form>
-		</>
-	);
-}
-
-
 function FormComp()
 {
-	const [user, setUser] = useState("User");
-	const [textIntput, setTextIntput] = useState("");
+	const [user, setUser] = useState(["user", "email"]);
+	const [textIntputName, setTextIntput] = useState(["", ""]);
 
 	function handlerChangeFormInput(event)
 	{
-		setTextIntput(event.target.value);
+		if (event.target.name == "user")
+		{
+			setTextIntput(textIntputName.map((value, index) => {
+					if (index === 0)
+						return value = event.target.value;
+					else if (index == 1)
+						return value;
+			}));
+			console.log(`handlerChangeFormInput for user: ${textIntputName[0]}`);
+		}
+		else if (event.target.name == "email")
+		{
+			setTextIntput(textIntputName.map((value, index) => {
+					if (index === 1)
+						return value = event.target.value;
+					else if (index == 0)
+						return value;
+			}));
+			console.log("handlerChangeFormInput for email: ", textIntputName[1]);
+		}
 	}
 
 	function handlerSubmitForm(event)
 	{
 		event.preventDefault();
-		setUser(textIntput);
+		console.log(`handlerSubmitForm. User: ${textIntputName[0]} // email: ${textIntputName[1]}`);
+		setUser([...textIntputName]);
 	}
 
 
 	return (
 		<>
-			<h1>Use event React with form</h1>
-			<FormFunc handlerInputChange={handlerChangeFormInput} handlerSubmit={handlerSubmitForm}/>
-			<p>{user}</p>
+			<h1>Use react event with form</h1>
+			<form onSubmit={handlerSubmitForm}>
+				<input name="user" onChange={handlerChangeFormInput}/>
+				<input name="email" onChange={handlerChangeFormInput}/>
+				<button type="submit">Submit</button>
+			</form>
+			<p>"User: "{user[0]}</p>
+			<p>"Email: "{user[1]}</p>
 		</>
 	);
 }
