a29a4a2ef97be4c367b6deceb71bcf2a162a7e65
mmychaly <mmychalyszyn@gmail.com>
2026-09-19 00:38:34 +0200

    add delete_image.php  avec fonctionalité de suppresion d'image

---
+++ projects/camagru/src/image/delete_image.php
@@ -0,0 +1,69 @@
+<?php
+
+require_once __DIR__  . '/../data/database.php';
+
+header('Content-Type: application/json; charset=utf-8');
+
+$imageId = filter_input(INPUT_POST, 'image_id', FILTER_VALIDATE_INT);//Recuperer le id de image depuis body de post
+
+if ($imageId === false || $imageId === null)//verification
+{
+		echo json_encode([
+		'success' => false,
+		'message' => 'Image invalide'
+	]);
+	exit;
+}
+
+//On va recuperer filename de image
+$stmt = $pdo->prepare(
+	'SELECT filename
+	FROM images
+	WHERE id = ? AND user_id = ?'
+);
+
+$stmt->execute([
+	$imageId,
+	$_SESSION['user_id']
+]);
+
+$image = $stmt->fetch();
+
+if (!$image)
+{
+	echo json_encode([
+		'success' => false,
+		'message' => 'Imposible de trouver l\'image!'
+	]);
+	exit;
+}
+
+$imgPath = __DIR__ . '/../../uploads/' . $image['filename']; //on utilise le filename pour faire le path jusqu'a fichier
+
+if (file_exists($imagePath))//Verification est ce que fichier existe
+{
+	if (!unlink($imagePath))//SI oui avec unlick on supprime le fichier
+	{
+		echo json_encode([
+			'success' => false,
+			'message' => 'Imposible de supprimer le fichier!'
+		]);
+		exit;
+	}
+}
+
+//On faire request vers db pour supprimer image 
+$stmt = $pdo->prepare(
+	'DELETE FROM images
+	WHERE id = ? AND user_id =?'
+);
+
+$stmt->execute([
+	$imageId,
+	$_SESSION['user_id']
+]);
+
+echo json_encode([
+	'success' => false,
+	'message' => 'Fichier supprimé'
+]);
